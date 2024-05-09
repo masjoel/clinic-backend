bb0fdef0a7daaf16eac2c10e3e230e834ec265d9
Masjoel <masjoel@gmail.com>
2024-05-09 13:38:58 +0700

    update Medical Rec Ctrl

--- app/Http/Controllers/Api/SatuSehatTokenController.php
+++ app/Http/Controllers/Api/SatuSehatTokenController.php
@@ -6,15 +6,21 @@ use App\Http\Controllers\Controller;
 
 class SatuSehatTokenController extends Controller
 {
+    protected $satusehatUrl;
+    protected $clientId;
+    protected $clientSecret;
+
+    public function __construct()
+    {
+        $this->satusehatUrl = config('satusehat.satusehat_url');
+        $this->clientId = config('satusehat.client_id');
+        $this->clientSecret = config('satusehat.client_secret');
+    }
     public function token()
     {
-        $satusehat_url = env('SATUSEHAT_BASE_URL_STG');
-        $client_id = env('CLIENTID_STG');
-        $client_secret = env('CLIENTSECRET_STG');
-        
         $curl = curl_init();
         curl_setopt_array($curl, array(
-            CURLOPT_URL => $satusehat_url.'/oauth2/v1/accesstoken?grant_type=client_credentials',
+            CURLOPT_URL => $this->satusehatUrl.'/oauth2/v1/accesstoken?grant_type=client_credentials',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
@@ -22,7 +28,7 @@ class SatuSehatTokenController extends Controller
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => 'client_id='.$client_id.'&client_secret='.$client_secret,
+            CURLOPT_POSTFIELDS => 'client_id='.$this->clientId.'&client_secret='.$this->clientSecret,
             CURLOPT_HTTPHEADER => array(
                 'Content-Type: application/x-www-form-urlencoded'
             ),
---
+++ config/satusehat.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'satusehat_url' => env('SATUSEHAT_BASE_URL_STG'),
+    'client_id' => env('CLIENTID_STG'),
+    'client_secret' => env('CLIENTSECRET_STG'),
+];
